066f9315bd3573aea2ac741417e87e0cf59f03bb
bedesmateo <147616966+bedesmateo@users.noreply.github.com>
2024-02-21 11:43:01 +0100

    Update TestBrides.py

--- TestBrides.py
+++ TestBrides.py
@@ -13,12 +13,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def testbrides( infile , Noise ):
-
-    #Where infile is the excel file containing the keysight data
     
-    df = pd.read_excel(infile) 
+df = pd.read_excel(infile) 
+    ConfigFile = open("Config_TB.json", "r")
+    jsonConfig = ConfigFile.read()
+    Config = json.loads(jsonConfig)
 
-    V0 = 4.3 #Volt
+    V0 = Config['V0'] #Volt
+    intHSchanels = Config['intHSchanels']
 
 
     Row1 = 0
@@ -34,12 +36,12 @@ def testbrides( infile , Noise ):
      #Number of col of the Datasample = Number of channels + Number of time scales (one for each channel)
      
     Call_CH = [] #List of the called channels
+    intCall_CH = []
     for i in range(1,int((Ncol+2)/2)-1):
         Call_CH = Call_CH + [df.iloc[Row1-1:Row1,2*i+1:2*i+2].iat[0,0]]
     for i in range(0,len(Call_CH)):
         Call_CH[i] = Call_CH[i][0:3]
-    print(Call_CH)
-
+        intCall_CH = intCall_CH + [int(Call_CH[i])]
 
     L_T=[ ] #To contain the time scales of each channel 
     L_VDC = [ ] #Signal of each channel
@@ -59,14 +61,10 @@ def testbrides( infile , Noise ):
     # Noise RMS
     RMS = []
     AVG = []
-
-    print(len(L_VDC[3]))
     for i in range(0,len(L_VDC)):
         RMS = RMS + [np.std(L_Vabs_NoOffset[i])]
         AVG = AVG + [np.mean(L_Vabs_NoOffset[i])]
-    print(RMS)
-    print(AVG)
-    print(np.log(RMS))
+    
 
     # Plot VDC(T) of each channel and |VDC-offset| of each channel
     for i in range(0,len(L_T)):
@@ -104,7 +102,6 @@ def testbrides( infile , Noise ):
         Pick_ind = Pick_ind + [ L_Vabs_NoOffset[i].idxmax() - Row1 ]
     for i in range(0,len(L_T)):
         T_Pick_ind = T_Pick_ind + [ L_T[i].iat[ Pick_ind[i][0] , 0 ] ]
-    print(Pick_ind[0])
 
 
     #For all the chanels
@@ -115,7 +112,6 @@ def testbrides( infile , Noise ):
     for i in range(0,len(L_VDC)):
         Square_wave = Square_wave + [[]]
         Square_wave_ind = Square_wave_ind + [[]]
-    print(Square_wave)
     for i in range(0,len(L_VDC)):
         if Pick_ind[i][0] -1 + 5 > len(df1) :
             d = len(df1) - ( Pick_ind[i][0] -1 )
@@ -140,24 +136,22 @@ def testbrides( infile , Noise ):
     for i in range(0,len(Square_wave)):
         Avg_SquareW = Avg_SquareW + [np.mean(Square_wave[i])]
         RMS_SquareW = RMS_SquareW + [np.std(Square_wave[i])]   
-    print('RMS_SquareW')
-    print(RMS_SquareW)
 
 
 
     # print( L_VDC[0].iat[Pick_ind[1][0] -1 ,0] )
 
-    L_Vaf = []  #For each channel i, calculates the value of the signal of the neighboor channels j != i just after the pick of the channel i 
-    L_Vbef = [] #For each channel i, calculates the value of the signal of the neighboor channels j != i just before the pick of the channel i
+    L_Vctalk = []  #For each channel i, calculates cross talk between i an j 
+    L_Vbreak = [] #For each channel i, calculates the value of the signal of the neighboor channels j != i during the break time
     L_Cor = [] #Quantify the correlation between a channel i and his neighboors
     for i in range(0,len(L_VDC)):
-        L_Vaf = L_Vaf + [[]]
-        L_Vbef = L_Vbef + [[]]
+        L_Vctalk = L_Vctalk + [[]]
+        L_Vbreak = L_Vbreak + [[]]
         L_Cor = L_Cor + [[]]
     for i in range(0,len(L_VDC)):
         for j in range(0,len(L_VDC)):
-            L_Vaf[i] = L_Vaf[i] + [0]
-            L_Vbef[i] = L_Vbef[i] + [0]
+            L_Vctalk[i] = L_Vctalk[i] + [0]
+            L_Vbreak[i] = L_Vbreak[i] + [0]
             L_Cor[i] = L_Cor[i] + [0]
             
     for i in range(0,len(L_VDC)):
@@ -165,34 +159,34 @@ def testbrides( infile , Noise ):
             if Square_wave_ind[i]==[]:
                 d = len(df1) - ( Pick_ind[i][0] )
                 for k in range(0,int(d)):
-                    L_Vaf[i][j] = L_Vaf[i][j] + L_Vabs_NoOffset[j].iat[ Pick_ind[i][0] +k, 0 ]
+                    L_Vctalk[i][j] = L_Vctalk[i][j] + L_Vabs_NoOffset[j].iat[ Pick_ind[i][0] +k, 0 ]
                 for k in range(0,EndBreak_ind-1):
-                    L_Vbef[i][j] = L_Vbef[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
-                L_Vaf[i][j] = L_Vaf[i][j] / int(d)
-                L_Vbef[i][j] = L_Vbef[i][j] / (EndBreak_ind-1)
+                    L_Vbreak[i][j] = L_Vbreak[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
+                L_Vctalk[i][j] = L_Vctalk[i][j] / int(d)
+                L_Vbreak[i][j] = L_Vbreak[i][j] / (EndBreak_ind-1)
                 
             else :
                 if Pick_ind[i][0] -1 + len(Square_wave[i]) > len(df1) :
                     d = len(df1) - ( Pick_ind[i][0] -1 )
                     for k in range(0,int(d)):
-                        L_Vaf[i][j] = L_Vaf[i][j] + L_Vabs_NoOffset[j].iat[ Square_wave_ind[i][0] +k, 0 ]
+                        L_Vctalk[i][j] = L_Vctalk[i][j] + L_Vabs_NoOffset[j].iat[ Square_wave_ind[i][0] +k, 0 ]
                         for k in range(0,EndBreak_ind-1):
-                            L_Vbef[i][j] = L_Vbef[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
-                        L_Vaf[i][j] = L_Vaf[i][j] / int(d)
-                        L_Vbef[i][j] = L_Vbef[i][j] / (EndBreak_ind-1)
+                            L_Vbreak[i][j] = L_Vbreak[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
+                        L_Vctalk[i][j] = L_Vctalk[i][j] / int(d)
+                        L_Vbreak[i][j] = L_Vbreak[i][j] / (EndBreak_ind-1)
                 
                 else:                 
                     for k in range(0,len(Square_wave[i])):
-                        L_Vaf[i][j] = L_Vaf[i][j] + L_Vabs_NoOffset[j].iat[ Square_wave_ind[i][0] +k, 0 ]
+                        L_Vctalk[i][j] = L_Vctalk[i][j] + L_Vabs_NoOffset[j].iat[ Square_wave_ind[i][0] +k, 0 ]
                     for k in range(0,EndBreak_ind-1):
-                        L_Vbef[i][j] = L_Vbef[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
-                    L_Vaf[i][j] = L_Vaf[i][j] / len(Square_wave[i])
-                    L_Vbef[i][j] = L_Vbef[i][j] / (EndBreak_ind-1)
+                        L_Vbreak[i][j] = L_Vbreak[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
+                    L_Vctalk[i][j] = L_Vctalk[i][j] / len(Square_wave[i])
+                    L_Vbreak[i][j] = L_Vbreak[i][j] / (EndBreak_ind-1)
 
         
     for i in range(0,len(L_VDC)):
         for j in range(0,len(L_VDC)):
-            L_Cor[i][j] = ( L_Vaf[i][j] - L_Vbef[i][j] ) / V0
+            L_Cor[i][j] = ( L_Vctalk[i][j] - L_Vbreak[i][j] ) / V0
 
 
     #For the functional chanels
@@ -205,13 +199,16 @@ def testbrides( infile , Noise ):
     Square_w_ind_in = []
     Avg_SquareW_in = [] # List of the average voltage over the SW of the diff chanels
     RMS_SquareW_in = []
-    L_Vaf_in = [] 
-    L_Vbef_in = [] 
+    L_Vctalk_in = [] 
+    L_Vbreak_in = [] 
     L_Cor_in = []
     L_Vabs_NoOffset_in = []
     L_T_in=[]
     for i in range(0,len(Call_CH)):
-        if Call_CH[i] != '102' and Call_CH[i] !='103' and Call_CH[i] !='104' and Call_CH[i] !='206' and Call_CH[i] !='207' and Call_CH[i] !='215':
+        A = int(Call_CH[i])
+        Bool = A in intHSchanels
+        if ( Bool == False ):
+             print(Call_CH[i] + ' is Valid')
              Amp_pick_in = Amp_pick_in + [Amp_pick[i]]
              Pick_ind_in = Pick_ind_in + [Pick_ind[i]]
              T_Pick_ind_in = T_Pick_ind_in + [T_Pick_ind[i]]
@@ -223,14 +220,16 @@ def testbrides( infile , Noise ):
              RMS_SquareW_in = RMS_SquareW_in + [RMS_SquareW[i]]
              L_Vabs_NoOffset_in = L_Vabs_NoOffset_in +[L_Vabs_NoOffset[i]]
              L_T_in = L_T_in + [L_T[i]]
+        else:
+            print(Call_CH[i] + ' is  invalid')
     for i in range(0,len(Call_CH_in)):
-        L_Vaf_in = L_Vaf_in + [[]]
-        L_Vbef_in = L_Vbef_in + [[]]
+        L_Vctalk_in = L_Vctalk_in + [[]]
+        L_Vbreak_in = L_Vbreak_in + [[]]
         L_Cor_in = L_Cor_in + [[]]
     for i in range(0,len(Call_CH_in)):
         for j in range(0,len(Call_CH_in)):
-            L_Vaf_in[i] = L_Vaf_in[i] + [0]
-            L_Vbef_in[i] = L_Vbef_in[i] + [0]
+            L_Vctalk_in[i] = L_Vctalk_in[i] + [0]
+            L_Vbreak_in[i] = L_Vbreak_in[i] + [0]
             L_Cor_in[i] = L_Cor_in[i] + [0]
             
     for i in range(0,len(Call_CH_in)):
@@ -238,39 +237,35 @@ def testbrides( infile , Noise ):
             if Square_w_ind_in[i]==[]:
                 d = len(df1) - ( Pick_ind_in[i][0] -1 )
                 for k in range(0,int(d)):
-                    L_Vaf_in[i][j] = L_Vaf_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Pick_ind_in[i][0] +k, 0 ]
+                    L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Pick_ind_in[i][0] +k, 0 ]
                 for k in range(0,EndBreak_ind-1):
-                    L_Vbef_in[i][j] = L_Vbef_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
-                L_Vaf_in[i][j] = L_Vaf_in[i][j] / int(d)
-                L_Vbef_in[i][j] = L_Vbef_in[i][j] / (EndBreak_ind-1)
+                    L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
+                L_Vctalk_in[i][j] = L_Vctalk_in[i][j] / int(d)
+                L_Vbreak_in[i][j] = L_Vbreak_in[i][j] / (EndBreak_ind-1)
                 
             else :
                 if Pick_ind_in[i][0] -1 + len(Square_w_in[i]) > len(df1) :
                     d = len(df1) - ( Pick_ind_in[i][0] -1 )
                     for k in range(0,int(d)):
-                        L_Vaf_in[i][j] = L_Vaf_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ]
+                        L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ]
                         for k in range(0,EndBreak_ind-1):
-                            L_Vbef_in[i][j] = L_Vbef_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
-                        L_Vaf_in[i][j] = L_Vaf_in[i][j] / int(d)
-                        L_Vbef_in[i][j] = L_Vbef_in[i][j] / (EndBreak_ind-1)
+                            L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
+                        L_Vctalk_in[i][j] = L_Vctalk_in[i][j] / int(d)
+                        L_Vbreak_in[i][j] = L_Vbreak_in[i][j] / (EndBreak_ind-1)
                 
                 else:                 
                     for k in range(0,len(Square_w_in[i])):
-                        L_Vaf_in[i][j] = L_Vaf_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ]
-                        if i ==0 and j ==0:
-                            print('i=j=0')
-                            print(L_Vabs_NoOffset_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ])
-                
+                        L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ]
                     for k in range(0,EndBreak_ind-1):
-                        L_Vbef_in[i][j] = L_Vbef_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
-                    L_Vaf_in[i][j] = L_Vaf_in[i][j] / len(Square_w_in[i])
-                    L_Vbef_in[i][j] = L_Vbef_in[i][j] / (EndBreak_ind-1)
+                        L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
+                    L_Vctalk_in[i][j] = L_Vctalk_in[i][j] / len(Square_w_in[i])
+                    L_Vbreak_in[i][j] = L_Vbreak_in[i][j] / (EndBreak_ind-1)
                     
 
     
     for i in range(0,len(Call_CH_in)):
         for j in range(0,len(Call_CH_in)):
-            L_Cor_in[i][j] = ( L_Vaf_in[i][j] - L_Vbef_in[i][j] ) / V0
+            L_Cor_in[i][j] = ( L_Vctalk_in[i][j] - L_Vbreak_in[i][j] ) / V0
 
     for i in range(0,len(L_T_in)):
         plt.plot(L_T_in[i],L_Vabs_NoOffset_in[i],label='Canal i')
@@ -296,7 +291,7 @@ def testbrides( infile , Noise ):
 
     #For the functional chanels
     DF_in = pd.DataFrame(L_Cor_in, Call_CH_in , Call_CH_in )
-    print('CORRELATION MATRIX FOR FUNCTIONAL CHANELS:')
+    print('              CORRELATION MATRIX FOR FUNCTIONAL CHANELS:')
     print(DF_in)
 
     res = sns.heatmap(np.log10(np.abs(DF_in)), vmin = -8 ,vmax = 0 , annot = True , annot_kws = {'fontsize': 7.9} , linewidths=1 , cbar = True , cmap = 'plasma')
@@ -309,14 +304,11 @@ def testbrides( infile , Noise ):
 
 
     #    AMPLITUDLE DATA FRAME PLOT
+    if Noise == False :
+        ampDF = pd.DataFrame( { 'Amplitude' : Avg_SquareW_in , 'RMS on the SW' : RMS_SquareW_in } , Call_CH_in )
 
-    colDF = []
-    for i in range(0,len(Call_CH)):
-        colDF = colDF + [str(Amp_pick[i][0])]
-    ampDF = pd.DataFrame( { 'Amplitude' : colDF } , Call_CH )
-
-    print('SIGNALS AMP:')
-    print(ampDF)
+        print('           SIGNALS AMP:')
+        print(ampDF)
         
 
     #    AMPLITUDE VS CHANELS PLOT
@@ -340,6 +332,7 @@ def testbrides( infile , Noise ):
     plt.errorbar(Call_CH_in, Avg_SquareW_in , RMS_SquareW_in , fmt='none', ecolor='red')
     plt.xlabel('Chanel Number')
     plt.ylabel('Amplitude')
+    plt.ylim(3.9,4.5)
     plt.legend(loc = 'upper right')
     plt.gcf().set_size_inches(20, 10)
     plt.title('Functional Chanels amplitude')
