47a94aea3678265d0e6d4ef02b6710b255437913
bedesmateo <147616966+bedesmateo@users.noreply.github.com>
2024-02-22 15:40:36 +0100

    Update TestBrides.py

--- TestBrides.py
+++ TestBrides.py
@@ -54,9 +54,9 @@ df = pd.read_excel(infile)
         L_Vabs_NoOffset = L_Vabs_NoOffset + [L_VDC[i]-L_VDC[i].iat[0,0]]
     for i in range(0,len(L_Vabs_NoOffset)):
         L_Vabs_NoOffset[i] = L_Vabs_NoOffset[i].abs()
-
     for i in range(0,len(L_Vabs_NoOffset)):
         L_Vabs_NoOffset[i] = L_Vabs_NoOffset[i].astype(float, errors="raise")
+
 
     # Noise RMS
     RMS = []
@@ -65,32 +65,39 @@ df = pd.read_excel(infile)
         RMS = RMS + [np.std(L_Vabs_NoOffset[i])]
         AVG = AVG + [np.mean(L_Vabs_NoOffset[i])]
     
+    #Converts the time coordonates in secondes
+    L_time = []
+    T0=3600 * float(str(L_T[0].iat[0,0])[11:13]) + 60 * float(str(L_T[0].iat[0,0])[14:16]) + float(str(L_T[0].iat[0,0])[17:])
+    for i in range (0,len(L_T)):
+        L_time = L_time + [[]]
+    for i in range (0,len(L_T)):
+        for j in range(0,len(L_T[i])):
+            H=float(str(L_T[i].iat[j,0])[11:13])
+            M=float(str(L_T[i].iat[j,0])[14:16])
+            S=float(str(L_T[i].iat[j,0])[17:])
+            T = 3600*H+60*M+S - T0
+            L_time[i] = L_time[i] + [T]
 
     # Plot VDC(T) of each channel and |VDC-offset| of each channel
     for i in range(0,len(L_T)):
-        plt.plot(L_T[i],L_VDC[i],label='Canal i')
+        plt.plot(L_time[i],L_VDC[i])
     plt.title('VDC')
-    plt.xlabel('Time')
+    plt.xlabel('Time (s)')
     plt.ylabel('VDC')  
     plt.show()
     plt.close()
-    for i in range(0,len(L_T)):
-        plt.plot(L_T[i],L_Vabs_NoOffset[i],label='Canal i')
-    plt.title('|VDC-offset|')
-    plt.xlabel('Time')
-    plt.ylabel('VDC')  
-    plt.show()
-    plt.close()
-
-    plt.plot(L_T[0],L_Vabs_NoOffset[0],label='Canal 101')
-    plt.plot(L_T[25],L_Vabs_NoOffset[25],label='Canal 210')
-    plt.plot(L_T[27],L_Vabs_NoOffset[27],label='Canal 212')
-    plt.legend()
-    plt.title('|VDC-offset|')
-    plt.xlabel('Time')
-    plt.ylabel('VDC')  
-    plt.show()
-    plt.close()
+    # for i in range(0,len(L_T)):
+    #     if intCall_CH[i] == 210:
+    #         plt.plot(L_time[i],L_Vabs_NoOffset[i],label='Channel 210')
+    #     else :
+    #         plt.plot(L_time[i],L_Vabs_NoOffset[i])
+    # plt.legend(loc = 'upper right')
+    # plt.title('|VDC-offset|')
+    # plt.xlabel('Time (s)')
+    # plt.ylim(-0.1,V0+0.2*V0)
+    # plt.ylabel('VDC')  
+    # plt.show()
+    # plt.close()
 
 
     Amp_pick = [ ] # Ampl of each channel's pick
@@ -103,6 +110,38 @@ df = pd.read_excel(infile)
     for i in range(0,len(L_T)):
         T_Pick_ind = T_Pick_ind + [ L_T[i].iat[ Pick_ind[i][0] , 0 ] ]
 
+    # print(type(L_VDC[0].iat[0,0]))
+    # print(type(L_VDC[0].iat[int(Pick_ind[0]),0]))
+
+    L_VDCnorm = []
+    for i in range(0,len(L_VDC)):
+        if L_VDC[i].iat[0,0] - L_VDC[i].iat[int(Pick_ind[i]),0] > 0:
+            L_VDCnorm = L_VDCnorm + [ ( -1 ) * L_VDC[i]]
+        else:
+            L_VDCnorm = L_VDCnorm + [ L_VDC[i] ]
+    for i in range(0,len(L_VDCnorm)):
+        L_VDCnorm[i] = L_VDCnorm[i] - L_VDCnorm[i].min()
+
+
+    for i in range(0,len(L_T)):
+        plt.plot(L_time[i],L_VDCnorm[i])
+    plt.title(' (-1) * VDC')
+    plt.xlabel('Time (s)')
+    plt.ylabel('VDC')  
+    plt.ylim(-0.1,V0+0.2*V0)
+    plt.show()
+    plt.close()
+
+    plt.plot(L_time[0],L_VDCnorm[0],label='Channel 101')
+    plt.plot(L_time[25],L_VDCnorm[25],label='Channel 210')
+    plt.plot(L_time[27],L_VDCnorm[27],label='Channel 212')
+    plt.legend()
+    plt.title(' (-1) * VDC')
+    plt.xlabel('Time (s)')
+    plt.ylabel('VDC')  
+    plt.ylim(-0.1,V0+0.2*V0)
+    plt.show()
+    plt.close()
 
     #For all the channels
     Square_wave = [] # Returns list of voltage of the differents points on the square wave of the differents channels 
@@ -116,12 +155,14 @@ df = pd.read_excel(infile)
         if Pick_ind[i][0] -1 + 5 > len(df1) :
             d = len(df1) - ( Pick_ind[i][0] -1 )
             for k in range( int(Pick_ind[i]) -d , int(Pick_ind[i]) + d -1):
-                if np.abs( float(L_Vabs_NoOffset[i].iat[k,0]) - float(Amp_pick[i]) ) < 2* float(RMS[i]) :
+                # if np.abs( float(L_Vabs_NoOffset[i].iat[k,0]) - float(Amp_pick[i]) ) < 2* float(RMS[i]) :
+                if np.abs( float(L_VDCnorm[i].iat[k,0]) - float(Amp_pick[i]) ) < 2* float(RMS[i]) :
                     Square_wave[i] = Square_wave[i] + [float(L_Vabs_NoOffset[i].iat[k,0])]
                     Square_wave_ind[i] = Square_wave_ind[i] + [k]
         else :
             for k in range( int(Pick_ind[i]) -5 , int(Pick_ind[i]) + 5):
-                if np.abs( float(L_Vabs_NoOffset[i].iat[k,0]) - float(Amp_pick[i]) ) < 2* float(RMS[i]) :
+                # if np.abs( float(L_Vabs_NoOffset[i].iat[k,0]) - float(Amp_pick[i]) ) < 2* float(RMS[i]) :
+                if np.abs( float(L_VDCnorm[i].iat[k,0]) - float(Amp_pick[i]) ) < 2* float(RMS[i]) :
                     Square_wave[i] = Square_wave[i] + [float(L_Vabs_NoOffset[i].iat[k,0])]
                     Square_wave_ind[i] = Square_wave_ind[i] + [k]
                     
@@ -136,7 +177,8 @@ df = pd.read_excel(infile)
     for i in range(0,len(Square_wave)):
         Avg_SquareW = Avg_SquareW + [np.mean(Square_wave[i])]
         RMS_SquareW = RMS_SquareW + [np.std(Square_wave[i])]   
-
+    print('AAAAAAA')
+    print(L_time[0][EndBreak_ind])
 
 
     # print( L_VDC[0].iat[Pick_ind[1][0] -1 ,0] )
@@ -159,9 +201,11 @@ df = pd.read_excel(infile)
             if Square_wave_ind[i]==[]:
                 d = len(df1) - ( Pick_ind[i][0] )
                 for k in range(0,int(d)):
-                    L_Vctalk[i][j] = L_Vctalk[i][j] + L_Vabs_NoOffset[j].iat[ Pick_ind[i][0] +k, 0 ]
+                    # L_Vctalk[i][j] = L_Vctalk[i][j] + L_Vabs_NoOffset[j].iat[ Pick_ind[i][0] +k, 0 ]
+                    L_Vctalk[i][j] = L_Vctalk[i][j] + L_VDCnorm[j].iat[ Pick_ind[i][0] +k, 0 ]
                 for k in range(0,EndBreak_ind-1):
-                    L_Vbreak[i][j] = L_Vbreak[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
+                    # L_Vbreak[i][j] = L_Vbreak[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
+                    L_Vbreak[i][j] = L_Vbreak[i][j] + L_VDCnorm[j].iat[ k , 0 ]
                 L_Vctalk[i][j] = L_Vctalk[i][j] / int(d)
                 L_Vbreak[i][j] = L_Vbreak[i][j] / (EndBreak_ind-1)
                 
@@ -169,17 +213,23 @@ df = pd.read_excel(infile)
                 if Pick_ind[i][0] -1 + len(Square_wave[i]) > len(df1) :
                     d = len(df1) - ( Pick_ind[i][0] -1 )
                     for k in range(0,int(d)):
-                        L_Vctalk[i][j] = L_Vctalk[i][j] + L_Vabs_NoOffset[j].iat[ Square_wave_ind[i][0] +k, 0 ]
+                        # L_Vctalk[i][j] = L_Vctalk[i][j] + L_Vabs_NoOffset[j].iat[ Square_wave_ind[i][0] +k, 0 ]
+                        L_Vctalk[i][j] = L_Vctalk[i][j] + L_VDCnorm[j].iat[ Square_wave_ind[i][0] +k, 0 ]
                         for k in range(0,EndBreak_ind-1):
-                            L_Vbreak[i][j] = L_Vbreak[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
+                            # L_Vbreak[i][j] = L_Vbreak[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
+                            L_Vbreak[i][j] = L_Vbreak[i][j] + L_VDCnorm[j].iat[ k , 0 ]
                         L_Vctalk[i][j] = L_Vctalk[i][j] / int(d)
                         L_Vbreak[i][j] = L_Vbreak[i][j] / (EndBreak_ind-1)
                 
                 else:                 
                     for k in range(0,len(Square_wave[i])):
-                        L_Vctalk[i][j] = L_Vctalk[i][j] + L_Vabs_NoOffset[j].iat[ Square_wave_ind[i][0] +k, 0 ]
+                        # L_Vctalk[i][j] = L_Vctalk[i][j] + L_Vabs_NoOffset[j].iat[ Square_wave_ind[i][0] +k, 0 ]
+                        L_Vctalk[i][j] = L_Vctalk[i][j] + L_VDCnorm[j].iat[ Square_wave_ind[i][0] +k, 0 ]
+
                     for k in range(0,EndBreak_ind-1):
-                        L_Vbreak[i][j] = L_Vbreak[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
+                        # L_Vbreak[i][j] = L_Vbreak[i][j] + L_Vabs_NoOffset[j].iat[ k , 0 ]
+                        L_Vbreak[i][j] = L_Vbreak[i][j] + L_VDCnorm[j].iat[ k , 0 ]
+
                     L_Vctalk[i][j] = L_Vctalk[i][j] / len(Square_wave[i])
                     L_Vbreak[i][j] = L_Vbreak[i][j] / (EndBreak_ind-1)
 
@@ -203,7 +253,8 @@ df = pd.read_excel(infile)
     L_Vbreak_in = [] 
     L_Cor_in = []
     L_Vabs_NoOffset_in = []
-    L_T_in=[]
+    L_VDCnorm_in = []
+    L_time_in=[]
     for i in range(0,len(Call_CH)):
         A = int(Call_CH[i])
         Bool = A in intHSchannels
@@ -219,7 +270,8 @@ df = pd.read_excel(infile)
              Avg_SquareW_in = Avg_SquareW_in + [Avg_SquareW[i]]
              RMS_SquareW_in = RMS_SquareW_in + [RMS_SquareW[i]]
              L_Vabs_NoOffset_in = L_Vabs_NoOffset_in +[L_Vabs_NoOffset[i]]
-             L_T_in = L_T_in + [L_T[i]]
+             L_VDCnorm_in = L_VDCnorm_in + [L_VDCnorm[i]]
+             L_time_in = L_time_in + [L_time[i]]
         else:
             print(Call_CH[i] + ' is  invalid')
     for i in range(0,len(Call_CH_in)):
@@ -237,9 +289,13 @@ df = pd.read_excel(infile)
             if Square_w_ind_in[i]==[]:
                 d = len(df1) - ( Pick_ind_in[i][0] -1 )
                 for k in range(0,int(d)):
-                    L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Pick_ind_in[i][0] +k, 0 ]
+                    # L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Pick_ind_in[i][0] +k, 0 ]
+                    L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_VDCnorm_in[j].iat[ Pick_ind_in[i][0] +k, 0 ]
+
                 for k in range(0,EndBreak_ind-1):
-                    L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
+                    # L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
+                    L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_VDCnorm_in[j].iat[ k , 0 ]
+
                 L_Vctalk_in[i][j] = L_Vctalk_in[i][j] / int(d)
                 L_Vbreak_in[i][j] = L_Vbreak_in[i][j] / (EndBreak_ind-1)
                 
@@ -247,17 +303,25 @@ df = pd.read_excel(infile)
                 if Pick_ind_in[i][0] -1 + len(Square_w_in[i]) > len(df1) :
                     d = len(df1) - ( Pick_ind_in[i][0] -1 )
                     for k in range(0,int(d)):
-                        L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ]
+                        # L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ]
+                        L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_VDCnorm_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ]
+
                         for k in range(0,EndBreak_ind-1):
-                            L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
+                            # L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
+                            L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_VDCnorm_in[j].iat[ k , 0 ]
+
                         L_Vctalk_in[i][j] = L_Vctalk_in[i][j] / int(d)
                         L_Vbreak_in[i][j] = L_Vbreak_in[i][j] / (EndBreak_ind-1)
                 
                 else:                 
                     for k in range(0,len(Square_w_in[i])):
-                        L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ]
+                        # L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_Vabs_NoOffset_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ]
+                        L_Vctalk_in[i][j] = L_Vctalk_in[i][j] + L_VDCnorm_in[j].iat[ Square_w_ind_in[i][0] +k, 0 ]
+
                     for k in range(0,EndBreak_ind-1):
-                        L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
+                        # L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_Vabs_NoOffset_in[j].iat[ k , 0 ]
+                        L_Vbreak_in[i][j] = L_Vbreak_in[i][j] + L_VDCnorm_in[j].iat[ k , 0 ]
+
                     L_Vctalk_in[i][j] = L_Vctalk_in[i][j] / len(Square_w_in[i])
                     L_Vbreak_in[i][j] = L_Vbreak_in[i][j] / (EndBreak_ind-1)
                     
@@ -267,11 +331,16 @@ df = pd.read_excel(infile)
         for j in range(0,len(Call_CH_in)):
             L_Cor_in[i][j] = ( L_Vctalk_in[i][j] - L_Vbreak_in[i][j] ) / V0
 
-    for i in range(0,len(L_T_in)):
-        plt.plot(L_T_in[i],L_Vabs_NoOffset_in[i],label='Canal i')
-    plt.title('|VDC-offset| for functional channels')
-    plt.xlabel('Time')
+    for i in range(0,len(L_time_in)):
+        if int(Call_CH_in[i]) == 210:
+            plt.plot(L_time_in[i],L_VDCnorm_in[i],label='Channel 210')
+        else :
+            plt.plot(L_time_in[i],L_VDCnorm_in[i])
+    plt.title(' (-1) * VDC for functional channels')
+    plt.xlabel('Time (s)')
     plt.ylabel('VDC')  
+    plt.ylim(-0.1,V0+0.2*V0)
+    plt.legend(loc = 'upper right')
     plt.show()
     plt.close()
 
@@ -283,7 +352,7 @@ df = pd.read_excel(infile)
     # print(DF)
 
     sns.heatmap(np.log10(np.abs(DF)), vmin = -8 ,vmax = 0 , annot = True , annot_kws = {'fontsize': 7.9} , linewidths=1 , cbar = True , cmap = 'plasma')
-    plt.title('Channels correlation matrix (log10)')
+    plt.title('Chanels correlation matrix (log10)')
     plt.gcf().set_size_inches(15, 10)
     plt.tight_layout()
     plt.show()
@@ -295,7 +364,7 @@ df = pd.read_excel(infile)
     print(DF_in)
 
     res = sns.heatmap(np.log10(np.abs(DF_in)), vmin = -8 ,vmax = 0 , annot = True , annot_kws = {'fontsize': 7.9} , linewidths=1 , cbar = True , cmap = 'plasma')
-    plt.title('Functional Chanels correlation matrix (log10)')
+    plt.title('Functional Channels correlation matrix (log10)')
     plt.gcf().set_size_inches(15, 10)
     res.set_xticklabels(res.get_xmajorticklabels(), fontsize = 10)
     plt.tight_layout()
@@ -316,7 +385,8 @@ df = pd.read_excel(infile)
     Mean=[]
     for i in range (0,len(Call_CH_in)):
         Mean=Mean+[np.mean(Avg_SquareW_in)]
-
+    print('               MEAN=')
+    print(Mean[0])
 
     plt.plot(Call_CH, Avg_SquareW , 'x' , color='blue')
     plt.errorbar(Call_CH, Avg_SquareW , RMS_SquareW , fmt='none', ecolor='red')
